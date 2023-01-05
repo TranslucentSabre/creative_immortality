0bc1802cfcb147419371ba9ad5b4664d613e7c7f
Timothy Myers <temyers240@gmail.com>
2023-01-04 22:32:19 -0600

    Refactor into a single function for core logic
    This function is able to operate on either a player name string
    or on a userdata object

--- init.lua
+++ init.lua
@@ -1,13 +1,14 @@
-function change_immortal_group(player, value) 
+function set_immortality(player, value)
+    -- Attempt to turn a string player name into a userdata object
+    if type(player) == "string" then
+        player = minetest.get_player_by_name(player)
+    end
+
+    -- If we were given a userdata object, or were able to retrieve one
+    if type(player) == "userdata" then
         local armor = player:get_armor_groups()
         armor.immortal = value
         player:set_armor_groups(armor)
-end
-
-function set_immortality(name, value)
-    local player = minetest.get_player_by_name(name)
-    if player then
-        change_immortal_group(player,value)
     end
 end
 
@@ -23,9 +24,8 @@ minetest.register_on_priv_revoke(function (name, granter, priv)
     end
 end)
 
-
-minetest.register_on_joinplayer(function (player,last_login)
+minetest.register_on_joinplayer(function (player, last_login)
     if minetest.check_player_privs(player, {creative=true}) then
-        change_immortal_group(player, 1)
+        set_immortality(player, 1)
     end
 end)
